eabfe4de013e834a8ad4cc896f21e8f2535abd8b
Patrick Tunezerwane <tunezepatrick@gmail.com>
2020-01-10 20:30:29 +0200

    add activate employee test

---
+++ test/employees/activateEmployee.js
@@ -0,0 +1,76 @@
+import ENV from 'dotenv';
+import chai from 'chai';
+import { describe, it } from 'mocha';
+import http from 'chai-http';
+import jwt from 'jsonwebtoken';
+import mockData from '../mockData/mockData';
+import app from '../../server';
+
+ENV.config();
+chai.use(http);
+chai.should();
+
+describe('when manager is trying to activate an employee ', () => {
+  it('should not be able to activate if no token provided', (done) => {
+    chai.request(app)
+      .put('/employees/1/activate')
+      .send({
+        status: 'active'
+      })
+      .end((err, res) => {
+        res.should.have.status(401);
+        done();
+      });
+  });
+  const token = jwt.sign(mockData, process.env.JWT_KEY, { expiresIn: '365d' });
+
+  it('should not be able to activate if there is an error in selection', (done) => {
+    chai.request(app)
+      .put('/employees/1/activate')
+      .set('x-auth-token', token)
+      .send({
+        status: '',
+      })
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.should.be.an('object');
+        res.body.should.have.property('status').eql(400);
+        res.body.should.have.property('error');
+        done();
+      });
+  });
+
+
+  it('should not be able to activate if an employee is not available', (done) => {
+    chai.request(app)
+      .put('/employees/800/activate')
+      .set('x-auth-token', token)
+      .send({
+        status: 'active'
+      })
+      .end((err, res) => {
+        res.should.have.status(404);
+        res.should.be.an('object');
+        res.body.should.have.property('status').eql(404);
+        res.body.should.have.property('error');
+        done();
+      });
+  });
+
+  it('should  be able to activate if an employee is found and a request is from manager ', (done) => {
+    chai.request(app)
+      .put('/employees/1/activate')
+      .set('x-auth-token', token)
+      .send({
+        status: 'active',
+      })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.an('object');
+        res.body.should.have.property('status').eql(200);
+        res.body.should.have.property('message');
+        res.body.should.have.property('data');
+        done();
+      });
+  });
+});
