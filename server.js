f42dd8a9cffbc76c54e39ad558cb4c4b670160e2
Patrick Tunezerwane <tunezepatrick@gmail.com>
2020-01-11 22:32:24 +0200

    add signin route

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv  from 'dotenv';
 import signupRouter from './server/routes/signup';
+import signinRouter from './server/routes/signin';
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use('/signup', signupRouter);
+app.use('/signin', signinRouter);
 const port = process.env.PORT || 5500;
 
 app.listen(port, () => {
