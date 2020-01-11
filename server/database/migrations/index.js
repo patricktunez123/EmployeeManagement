5aa8daf8bc9e01bb7cc53e32e74018a3c1068696
Patrick Tunezerwane <tunezepatrick@gmail.com>
2020-01-11 19:32:34 +0200

    export migrations

---
+++ server/database/migrations/index.js
@@ -0,0 +1,7 @@
+import CreateEmployeesTable from './create_employees_table';
+import CreateManagersTable from './create_managers_table';
+
+export {
+  CreateEmployeesTable,
+  CreateManagersTable,
+};
