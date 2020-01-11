60f5932c8a16af255eef4b6321b34c79bea52dc9
Patrick Tunezerwane <tunezepatrick@gmail.com>
2020-01-11 23:16:51 +0200

    add employee route

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ import express from 'express';
 import dotenv  from 'dotenv';
 import signupRouter from './server/routes/signup';
 import signinRouter from './server/routes/signin';
+import employeeRouter from './server/routes/registerEmployee';
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use('/signup', signupRouter);
 app.use('/signin', signinRouter);
+app.use('/employees', employeeRouter);
 const port = process.env.PORT || 5500;
 
 app.listen(port, () => {
