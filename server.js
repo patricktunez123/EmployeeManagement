607aabc2e3466223685fc9fab6d237836f133d96
Patrick Tunezerwane <tunezepatrick@gmail.com>
2020-01-11 21:52:47 +0200

    add signup route

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import dotenv  from 'dotenv';
+import signupRouter from './server/routes/signup';
 
 dotenv.config();
 const app = express();
 app.use(express.json());
-
+app.use('/signup', signupRouter);
 const port = process.env.PORT || 5500;
 
 app.listen(port, () => {
