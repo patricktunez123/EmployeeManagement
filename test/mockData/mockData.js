982f21e91789dedced4624b5c225d887e395fa7f
Patrick Tunezerwane <tunezepatrick@gmail.com>
2020-01-13 01:09:37 +0200

    refactor

--- test/mockData/mockData.js
+++ test/mockData/mockData.js
@@ -1,15 +1,4 @@
 const mockData = {
-  managerPayload: {
-    id: 2,
-    name: 'Patrick Tunezerwane',
-    nationalID: '1234345612456786',
-    phoneNumber: '0781429268',
-    dateOfBirth: '01.01.1999',
-    status: 'active',
-    position: 'Manager ',
-    email: 'tp3@gmail.com',
-    password: '$2b$10$Atnw/KEDHvmcSdNTRWfMfOZIOOQFOIynwjiYqGGZx3xtemaF6NGe6'
-  },
   canLogin: {
     email: 'pazo20@gmail.com',
     password: '@12345@23',
