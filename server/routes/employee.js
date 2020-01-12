e44881952a9da57083cc3cf4ba87350e60b77bed
Patrick Tunezerwane <tunezepatrick@gmail.com>
2020-01-12 18:06:17 +0200

    add activate employee route

--- server/routes/employee.js
+++ server/routes/employee.js
@@ -2,6 +2,7 @@ import express from 'express';
 import StoreEmployeeController from '../controllers/StoreEmployeeController';
 import DeleteEmployeeController from '../controllers/DeleteEmployeeController';
 import EditEmployeeController from '../controllers/EditEmployeeController';
+import ActivateEmployeeController from '../controllers/ActivateEmployeeController';
 import auth from '../middlewares/auth';
 
 const router = express.Router();
@@ -9,4 +10,5 @@ const router = express.Router();
 router.post('/', auth, StoreEmployeeController);
 router.delete('/:id/', auth, DeleteEmployeeController);
 router.put('/:id/', auth, EditEmployeeController);
+router.put('/:id/activate/', auth, ActivateEmployeeController);
 export default router;
