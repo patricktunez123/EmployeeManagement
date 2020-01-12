4e1566aebff291c06bd198ca7a172e243256c3f7
Patrick Tunezerwane <tunezepatrick@gmail.com>
2020-01-12 14:12:43 +0200

    add delete employee route

--- server/routes/employee.js
+++ server/routes/employee.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import StoreEmployeeController from '../controllers/StoreEmployeeController';
+import DeleteEmployeeController from '../controllers/DeleteEmployeeController';
 import auth from '../middlewares/auth';
 
 const router = express.Router();
 
 router.post('/', auth, StoreEmployeeController);
+router.delete('/:id/', auth, DeleteEmployeeController);
 
 export default router;
