0a4611cb055b5c7f05732388a965d387951ac0c1
Patrick Tunezerwane <tunezepatrick@gmail.com>
2020-01-13 12:39:12 +0200

    import swagger-ui-express and setup swagger documentation

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv  from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import swaggerdoc from './swagger.json';
 import signupRouter from './server/routes/signup';
 import signinRouter from './server/routes/signin';
 import employeeRouter from './server/routes/employee';
 
 dotenv.config();
 const app = express();
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerdoc));
 app.use(express.json());
 app.use('/signup', signupRouter);
 app.use('/signin', signinRouter);
