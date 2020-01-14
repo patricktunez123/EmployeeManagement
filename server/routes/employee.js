738f0131fa8e259239936eb72d56b6d9b75b6f7e
Patrick Tunezerwane <tunezepatrick@gmail.com>
2020-01-14 12:21:35 +0200

    (refactor) remove paths to removed controllers

--- server/routes/employee.js
+++ server/routes/employee.js
@@ -1,18 +1,13 @@
 import express from 'express';
-import StoreEmployeeController from '../controllers/StoreEmployeeController';
-import DeleteEmployeeController from '../controllers/DeleteEmployeeController';
-import EditEmployeeController from '../controllers/EditEmployeeController';
-import ActivateEmployeeController from '../controllers/ActivateEmployeeController';
-import SuspendEmployeeController from '../controllers/SuspendEmployeeController';
-import SearchController from '../controllers/SearchController';
+import EmployeeController from '../controllers/EmployeeController';
 import auth from '../middlewares/auth';
 
 const router = express.Router();
 
-router.post('/', auth, StoreEmployeeController);
-router.delete('/:id/', auth, DeleteEmployeeController);
-router.put('/:id/', auth, EditEmployeeController);
-router.put('/:id/activate/', auth, ActivateEmployeeController);
-router.put('/:id/suspend/', auth, SuspendEmployeeController);
-router.post('/search', auth, SearchController);
+router.post('/', auth, EmployeeController.store);
+router.delete('/:id/', auth, EmployeeController.delete);
+router.put('/:id/', auth, EmployeeController.edit);
+router.put('/:id/activate/', auth, EmployeeController.activate);
+router.put('/:id/suspend/', auth, EmployeeController.suspend);
+router.post('/search', auth, EmployeeController.search);
 export default router;
